8bd21c5f0248fa110055bbcdc565c79da29ce941
Cassidy Helms <cassidy.helms@gmail.com>
2025-05-22 16:38:43 -0400

    retrieve response data at response.data

--- src/services/searchNearbyService.js
+++ src/services/searchNearbyService.js
@@ -5,7 +5,7 @@ const API_URL = "http://localhost:8080/search";
 export const searchNearbyRestaurants = async () => {
   try {
     const response = await axios.get(API_URL);
-    return response.data.places || [];
+    return response.data || [];
   } catch (error) {
     console.error("Error searching nearby restaurants:", error);
     throw error;
